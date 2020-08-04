b9cd3cb1d4ae813bf18a0b6a6d64562e4ca29974
gaarahan <gaarahan@foxmail.com>
2020-08-04 15:43:08 +0800

    [Gaarahan] resolve stub

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,24 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GradeServiceTest {
-    /* 需求描述：
-    编写GradeService类的单元测试，单元测试calculateAverageGrades方法
-    * */
+  /* 需求描述：
+  编写GradeService类的单元测试，单元测试calculateAverageGrades方法
+  * */
+  GradeService gradeService;
+  @BeforeEach
+  void setup () {
+    gradeService = new GradeService(new MockGradeSystem());
+  }
 
-    @Test
-    public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
-    }
+  @Test
+  public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
+    double result = gradeService.calculateAverageGrades(10002);
+    Assertions.assertEquals(90.0, result);
+
+  }
 }
